ec82ae2d3b52bb9ab8c8ab9c16c8b3a740e31ccc
Kaline Marques <134382161+kalinemarques@users.noreply.github.com>
2023-12-05 16:26:43 -0300

    Update menu.py

--- viewer/menu.py
+++ viewer/menu.py
@@ -1,83 +1,100 @@
+import os
 import getpass
-from sigerpe.viewer import valida
-
-gerentes = {
-    'kaliane': {'kaliane@gmail.com': 'amoGoiaba'},
-    'daniel': {'daniel@gmail.com': '12345678'},
-    'kaline': {'kaline@gmail.com': 'Qw78@.'}
-}
-
-def opcao2():
-    email = input('Email: ')
-    if valida.check(email):
-        valida.verificasenha()
-        return True
-    else:
-        print("Email inválido, por favor digite novamente.")
-        email = input('Email: ')
-
+from viewer import valida
+from controller.mesaController import CadastrarMesa, ExcluirMesa, BuscarMesa, ListarMesa, AlterarMesa
+ 
 def opcao1():
-    usuario = input('Nome de Usuário: ')
+    usuario = input('Nome de Usuário: ').lower()
     senha = getpass.getpass('Senha: ')
-    #VERIFICAR A LISTA DE CLIENTES
-    if (senha == 'abc') and (usuario == 'joana'):
-        verifica = True
-        print('verificou')
-    
-    if verifica==True:
-        print("entrou")
+    teste = valida.validaEntrada(usuario, senha) 
+    if teste == True:
         while True:
             escolha = input("""
-                                SiGerPe
-        Sistema de Gerenciamento de Pedidos para restaurantes
-        
-
-        ___GERENCIAMENTO DE MESAS___                
-
-        [1] - Cadastrar mesa.
-        [2] - Aletar mesa. 
-        [3] - Listar mesas.  
-        [4] - Buscar mesa.                                                            
-        [5] - Deletar mesa.
-        [6] - Sair.
+         _______________________________________________________                    
+        |                                                       | 
+        |                         SiGerPe                       |
+        | Sistema de Gerenciamento de Pedidos para restaurantes |
+        |_______________________________________________________|
+        |                                                       |
+        |              ___GERENCIAMENTO DE MESAS___             |   
+        |                                                       |
+        |  [1] - Cadastrar mesa.                                |
+        |  [2] - Alterar mesa.                                  |
+        |  [3] - Listar mesas.                                  |
+        |  [4] - Buscar mesa.                                   |                                      
+        |  [5] - Deletar mesa.                                  |
+        |  [6] - Voltar.                                        |
+        |  [7] - Sair.                                          |
+        |_______________________________________________________|
         """)
-            if escolha == '1':
-                pass
-            if escolha =='2':
-                pass
-            if escolha == '3':
-                pass
-            if escolha =='4':
-                pass
-            if escolha == '5':
-                pass
-            if escolha =='6':
-                break
-            else:
-                print("Opção inválida!")
+            os.system('cls')
+            try:
+                if escolha == '1':
+                    print('___CADASTRO DE MESAS___\n')
+                    qtdMesa = int(input("Insira capacidade de pessoas na mesa: "))
+                    idGarcom = int(input("Insira o id do garçom: "))
+                    CadastrarMesa.post(qtdMesa, idGarcom)
+                    print("Mesa cadastrada com sucesso!")
+                if escolha == '2':
+                    print('___ALTERAR MESA___\n')
+                    idGarcom = int(input("Insira o id do novo garçom: "))
+                    idMesa = int(input("Insira o id da mesa: "))                
+                    AlterarMesa.get(idMesa, idGarcom) 
+                    print("Update realizado com sucesso!")              
+                if escolha == '3':
+                    print("___Lista de mesas___\n")
+                    ListarMesa.get()
+                if escolha == '4':
+                    print("___Busca___\n")
+                    idMesa = int(input("Insira o id da mesa: "))
+                    BuscarMesa.get(idMesa)
+                    
+
+                if escolha == '5':
+                    print("___Deletar___\n")
+                    idMesa = int(input("Insira o ID da mesa: "))
+                    ExcluirMesa.get(idMesa)
+                    print("Mesa deletada com sucesso!")
+                if escolha == '6':
+                    return False
+                if escolha == '7':
+                    break
+                if escolha not in {'1', '2', '3', '4', '5', '6', '7'}:
+                    print("Opção inválida!")
+            except IndexError:
+                print("Mesa não encontrada no banco de dados.")
+            except ValueError:
+                print("Por favor, insira um ID de mesa válido (número inteiro).")
         return True
-        
+
 escolha = input("""
-                            SiGerPe
-      Sistema de Gerenciamento de Pedidos para restaurantes
-      
-
-                
-
-    [1] - Entrar.    
-    [2] - Recuperar senha.
-    [3] - Sair.
+         _______________________________________________________                    
+        |                                                       | 
+        |                        SiGerPe                        |
+        | Sistema de Gerenciamento de Pedidos para restaurantes |
+        |_______________________________________________________|
+        |                                                       |                
+        |                     ___LOGIN___                       |
+        |                                                       |                
+        | [1] - Entrar.                                         |
+        | [2] - Sair                                            |
+        |                                                       |
+        |_______________________________________________________|        
       """)
 verifica = False
+os.system('cls')
+
 while True:
     if escolha == '1':
         if opcao1():
             break
 
-    if escolha == '2':       
-        if opcao2():
-            break
+    """ if escolha == '2':       
+        if not opcao2():
+            break"""
 
-    if escolha == '3':
+    if escolha == '2':
         print("Saindo do sistema...")
         break
+    if escolha not in {'1', '2'}:
+        print("Opção inválida.")
